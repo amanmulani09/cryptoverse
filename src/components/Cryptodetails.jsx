1e4259004a2add800ad4186cc42d928c9da49375
Aman Mulani <mulaniman0504@gmail.com>
2022-12-28 21:37:25 +0530

    initial commit worked on layout and package.

---
+++ src/components/Cryptodetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cryptodetails = () => {
+  return (
+    <div>Cryptodetails</div>
+  )
+}
+
+export default Cryptodetails
---
+++ src/components/component.js
@@ -0,0 +1,7 @@
+import Navbar from './Navbar'
+import News from './News'
+import Cryptocurrencies from './Cryptocurrencies'
+import Cryptodetails from './Cryptodetails'
+import Exchanges from './Exchanges'
+import Homepage from './Homepage'
+export {Navbar,News,Cryptocurrencies,Cryptodetails,Exchanges,Homepage}
